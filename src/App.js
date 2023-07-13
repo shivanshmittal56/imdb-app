86e6cfad5485904a0cc36a6dbb1bfcc451df0267
Shivansh Mittal <mittalshivansh56@gmail.com>
2023-07-13 11:10:00 +0530

    first version of movie app

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import Header from "./components/header/header"
+import Home from "./pages/home/home"
+import MovieList from './components/movieList/movieList';
+import MovieDetail from './pages/movieDetail/movieDetail';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Router>
+      <Header/>
+      <Routes>
+        <Route index element={<Home/>}></Route>
+        <Route path="movie/:id" element={<MovieDetail/>}></Route>
+        <Route path="movies/:type" element={<MovieList/>}></Route>
+        <Route path="/*" element={<h1>Error page</h1>}></Route>
+      </Routes>
+     </Router>
+    </div>
+  );
+}
+
+export default App;
+
